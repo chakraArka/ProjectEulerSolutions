770498341d444a2793997f5e87d3a75cc959cd80
chakraArka <arka.chakra24@gmail.com>
2023-05-24 17:35:58 +0530

    ...

--- SmallestMultiple.py
+++ SmallestMultiple.py
@@ -1,15 +1,16 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers
 # from 1 to 20?
 
+numbers = 20
 # Start with the current minimum number = 2,
 # since it is common knowledge that 2 is divisible by both 2 and 1.
 currMinNum = 2
 # nextNum represents the next integer that currMinNum has to be divisible by.
 nextNum = 3
-while nextNum <= 20:
+while nextNum <= numbers:
     temp = currMinNum
     addInt = nextNum - 1
-    while(currMinNum % temp != 0 or currMinNum % nextNum != 0):
+    while currMinNum % temp != 0 or currMinNum % nextNum != 0:
         currMinNum += addInt
     nextNum += 1
 print("Smallest Positive Number that is evenly divisible by all numbers from 1 to 20 :")
