2306b6da6dea6f29fe3dbdbd33453fe15969cbe3
chakraArka <arka.chakra24@gmail.com>
2023-05-25 09:52:20 +0530

    Modified output and added 'maxValue' variable

--- EvenFibonacciNumbers.py
+++ EvenFibonacciNumbers.py
@@ -4,12 +4,13 @@
 nextFibonacciNumber = 2
 prevFibonacciNumber = 1
 fibSum = 0
-while nextFibonacciNumber <= 4000000:
+maxValue = 4000000
+while nextFibonacciNumber <= maxValue:
     temp = nextFibonacciNumber
     nextFibonacciNumber = nextFibonacciNumber + prevFibonacciNumber
     prevFibonacciNumber = temp
     if prevFibonacciNumber % 2 == 0:
         fibSum += prevFibonacciNumber
 
-print("Sum of even-valued terms in Fibonacci sequence (values less than four million)")
+print("Sum of even-valued terms in Fibonacci sequence (values less than " + str(maxValue) + ") = ", end="")
 print(fibSum)
